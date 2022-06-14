f8d5fed2748bf8d7cedbc99fcb583be9a2b3eb7b
John Knoernschild <procisco@gmail.com>
2022-06-14 18:46:07 -0400

    refactored task 2

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 
 expenses = Expense.Expenses()
-expenses.read_expenses(filename='data/spending_data.csv')
+expenses.read_expenses('data/spending_data.csv')
 spending_categories = []
 
 
